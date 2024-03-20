47e2d9ee498fdb52c1ffd3c9d6f0eb6754e1aa54
Lambrous <lambrous@outlook.ph>
2024-03-20 14:44:56 +0800

    Implement basic math operations

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let firstNum;
+let secondNum;
+let operator;
+
+function operate(num1, num2, operator) {
+	switch (operator) {
+		case "+":
+			return add(num1, num2);
+		case "-":
+			return subtract(num1, num2);
+		case "*":
+			return multiply(num1, num2);
+		case "/":
+			return divide(num1, num2);
+	}
+}
